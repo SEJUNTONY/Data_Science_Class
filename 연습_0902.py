6b31072d3c9075e2d805272b5e0e0e4c4dd79dd0
SEJUNTONY <tonypark0723@gmail.com>
2021-09-02 23:02:45 +0900

    9월 2일 수업입니다.

---
+++ 연습_0902.py
@@ -0,0 +1,29 @@
+# 1번 문제 #
+
+import turtle
+
+## 함수 선언 부분 ##
+
+## 변수 선언 부분 ##
+myT = None
+
+## 메인 코드 부분 ##
+turtle.shape('turtle')
+
+turtle.forward(300)
+turtle.right(120)
+turtle.forward(300)
+turtle.right(120)
+turtle.forward(300)
+
+turtle.done()
+
+# 2번 문제 #
+
+a = 2
+b = 3
+
+result1 = a+b
+result2 = a*b
+result3 = pow(a,b)
+print(result1, result2, result3)
