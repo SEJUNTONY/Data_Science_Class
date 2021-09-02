1be22f8b24a1e1ce52aa4a95d0ea95973f380d14
EunJi1849 <89200235+EunJi1849@users.noreply.github.com>
2021-09-02 23:43:14 +0900

    9월2일_은지연습

--- 은지연습_0902.py
+++ 은지연습_0902.py
@@ -1,17 +1,4 @@
-#1
-import turtle
-
-myT = None
-
-turtle.shape('turtle')
-
-turtle.forward(100)
-turtle.right(240)
-turtle.forward(100)
-turtle.right(240)
-turtle.forward(100)
-
-#1
+# 1
 import turtle
 
 myT = None
@@ -27,18 +14,7 @@ turtle.forward(100)
 turtle.done()
 
 
-#2
-p = input("숫자를 입력하세요")
-q = input("숫자를 입력하세요")
-
-result1 = int(p) + int(q)
-result2 = int(p) * int(q)
-result3 = pow(int(p),int(q))
-
-print(result1, result2, result3).done()
-
-
-#2
+# 2
 p = input("숫자를 입력하세요")
 q = input("숫자를 입력하세요")
 
