676cdf190672fd974bda4a7a1b502f4a53162c09
eliteuserfox <89200571+eliteuserfox@users.noreply.github.com>
2021-09-02 23:19:00 +0900

    9월 2일 실습입니당!!

---
+++ 연습_0902.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+turtle.right(120)
+turtle.done()
+
+a = input()
+b = input()
+result1 = pow(int(a),int(b))
+result2 = int(a) + int(b)
+result3 = int(a) * int(b)
+
+print(result1)
+print(result2)
+print(result3)
