cc79e645663403995adf9638a526390ebfb18c84
Eu72o1 <89200471+Eu72o1@users.noreply.github.com>
2021-09-02 23:07:36 +0900

    0921 실습입니다.

---
+++ 연습_09021.py
@@ -0,0 +1,26 @@
+import turtle
+
+## 함수 선언 부분 ##
+
+## 변수 선언 부분 ##
+myT = None
+
+## 메인 코드 부분 ##
+turtle.shape('turtle')
+
+turtle.forward(300)
+turtle.right(120)
+turtle.forward(300)
+turtle.right(120)
+turtle.forward(300)
+
+turtle.done()
+
+
+a=2
+b=3
+
+result1=a+b
+result2=a*b
+result3=pow(a,b)
+print(result1, reseult2, reseult3)
