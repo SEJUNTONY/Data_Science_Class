03bfe8c1d372dccd3f00e065cd728acc964786c7
JISOOBEEN <gndb13@naver.com>
2021-09-02 23:02:47 +0900

    9월 2일 수업입니다

---
+++ 연습_0902터틀.py
@@ -0,0 +1,30 @@
+import turtle
+
+## 함수 선언 부분 ##
+
+## 변수 선언 부분 ##
+myT = None
+
+## 메인 코드 부분 ##
+turtle.shape('turtle')
+
+#직각삼각형
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(135)
+turtle.forward(300)
+#turtle.right(90)
+#turtle.forward(200)
+
+#정삼각형
+turtle.penup
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+
+turtle.done()
+
